a7bccb2eafd630610a90a4aa14f2e144bd6cc5c2
jnardini <jn96college@gmail.com>
2023-06-19 10:03:50 -0400

    added hello resource

--- application.py
+++ application.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Hello Don Piano!'
+    return 'Home Page'
+
+@application.route('/hello', methods=['GET'])
+def hello_world():
+    name = request.args.get('name', 'Schatzi')
+    return f'Hello {name}!'
