b5896b0211e7096c52a80ff70ef4d2952f22e07e
jnardini <jn96college@gmail.com>
2023-06-25 16:34:29 -0400

    added json body test

--- application.py
+++ application.py
@@ -10,3 +10,14 @@ def home():
 def hello_world():
     name = request.args.get('name', 'Schatzi')
     return f'Hello {name}!'
+
+@application.route('/test-json', methods=['POST'])
+def handle_post():
+    data = request.get_json()
+    email = data['email']
+    datetime = data['datetime']
+    questionnaire = data['questionnaire']
+    context = data['context']
+    # Pass data to your function(s) here
+
+    return f'Hello {email}, {datetime}, {questionnaire}, {context}!', 200
